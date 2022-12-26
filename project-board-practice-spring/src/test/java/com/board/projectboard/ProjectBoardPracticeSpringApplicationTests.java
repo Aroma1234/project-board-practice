58ce8370cfb3301f4c833f783f610993ae0755b6
jiwonjjang <ukkangsolo@gmail.com>
2022-12-26 15:11:36 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은 :
    
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ project-board-practice-spring/src/test/java/com/board/projectboard/ProjectBoardPracticeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBoardPracticeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
